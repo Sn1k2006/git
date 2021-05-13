209c792e76faec21a837eb74678ea184dcad7611
sn1k2006 <sn1k2006@mail.ru>
2021-05-13 16:36:21 +0300

    dddd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.body.innerHTML = '<h1>Hello from js</h1>'
 console.log('test 2');
 console.log('test 1');
+console.log('test as3');
 console.log('test 3');
-console.log('test 3');
-console.log('test r');
+console.log('testdd');
